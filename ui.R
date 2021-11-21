ebf79f1791cf33e72e4f1f2838c092f252b81735
andreaglopez <Andreagil951@gmail.com>
2021-11-21 14:18:21 -0500

    removed selectInput and instead added uiOutput. You can now see list of senators now

--- ui.R
+++ ui.R
@@ -16,25 +16,14 @@ ui <- fluidPage(
   
   sidebarPanel(
       helpText("Using U.S Politicans' tweets to survey vaccine rate"),
-      selectInput("Choosing variables",
-                  label = "Choose a U.S Sentator from the list",
-                  choices = c("some name", "other name")
-      ),
+      uiOutput('senators'),
       
       sliderInput("a",
                   label = "Select value to view top common words",
-                  min = 1, max = 10, value = 5)
-      ),
+                  min = 1, max = 10, value = 5),
+  ),
   
-  
-  mainPanel( "main panel"
-    
-  )
-  
-  
+  mainPanel( "main panel")
   
   
 )
-
-
-
