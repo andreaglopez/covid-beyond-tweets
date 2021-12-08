6a8a2b271d3a643df7c93b370d6d21f04362f677
Andrea <35052973+andreaglopez@users.noreply.github.com>
2021-12-08 14:26:57 -0500

    new ui.R file

--- ui.R
+++ ui.R
@@ -1,29 +1,55 @@
 
 ## Connecting 
 #install.packages('rsconnect')
+#install.packages('evaluate')
+#install.packages('markdown')
 
 ##the face
 
 library(shiny)
+library(shinydashboard)
 library(dplyr)
 library(rtweet)
 library(rsconnect)
-library
+library(evaluate)
+library(markdown)
 
-ui <- fluidPage(
-  titlePanel("Beyond The Tweets"),
-  
-  
-  sidebarPanel(
-      helpText("Using U.S Politicans' tweets to survey vaccine rate"),
-      uiOutput('senators'),
-      
-      sliderInput("a",
-                  label = "Select value to view top common words",
-                  min = 1, max = 10, value = 5),
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Beyond The Tweets"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Main", tabName = "Main", icon = icon("r-project")),
+    menuItem("ReadMe", tabName = "ReadMe", icon = icon("readme"))
+    )
   ),
   
-  mainPanel( "main panel")
+  dashboardBody(
+  tabItems(
+    tabItem(
+      tabName = "Main",
   
-  
+      sidebarPanel(
+        helpText("Using U.S Politicans' tweets"),
+        uiOutput('senator'),
+        
+        sliderInput(
+          "a",
+          label = "Select value to view top common words",
+          min = 1,
+          max = 10,
+          value = 5
+          ),
+        ),
+      
+      
+    mainPanel(
+      plotOutput("plot")
+    )
+     
+     
+    ),
+    tabItem(tabName = "ReadMe", 
+            includeMarkdown("README.md"))
+    ),
+  )
 )
